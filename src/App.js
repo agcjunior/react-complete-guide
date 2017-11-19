b7fd0695f69d2d061b7469a5c54886f51a977c5a
AJ <antonio.jr@outlook.com>
2017-11-19 10:26:13 -0300

    bug fixes

--- src/App.js
+++ src/App.js
@@ -20,13 +20,20 @@ class App extends Component {
     })
   }
   
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: event.target.value, age: 56},
-        { name: 'Maria do Carmo', age: 58}
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person =  {
+      ...this.state.persons[personIndex]
+    }   
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons : persons} );
   }
 
   togglePersonsHandler = () => {
@@ -64,7 +71,8 @@ class App extends Component {
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
-                key={person.id} />
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)} />
             })
           }          
         </div>
